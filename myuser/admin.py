d3c5139292f3ae3ef7d49240fa9e03dca194161b
Stepanyan <you@example.com>
2016-07-02 15:54:29 +0400

    Admin

--- myuser/admin.py
+++ myuser/admin.py
@@ -1,3 +1,56 @@
+from django import forms
+from simplemathcaptcha.fields import MathCaptchaField
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import MyUser
 
-# Register your models here.
+
+class UserCreationForm(forms.ModelForm):
+		password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'}))
+		password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'confirm password'}))
+		captcha = MathCaptchaField()
+		class Meta:
+			model = MyUser
+			fields = ('email', 'date_of_birth')
+		def __init__(self, *args, **kwargs):
+				super(UserCreationForm,self).__init__(*args,**kwargs)
+				self.fields['date_of_birth'].widget.attrs={'class':'form-control','placeholder':'Date of birth'}
+				self.fields['email'].widget.attrs={'class':'form-control','placeholder':'Email'}
+		def clean_password2(self):
+			password1 = self.cleaned_data.get("password1")
+			password2 = self.cleaned_data.get("password2")
+			if password1 and password2 and password1 != password2:
+					raise forms.ValidationError("Passwords don't match")
+			return password2
+		def save(self, commit=True):
+			user = super(UserCreationForm, self).save(commit=False)
+			user.set_password(self.cleaned_data["password1"])
+			if commit:
+					user.save()
+			return user
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    add_form = UserCreationForm
+    list_display = ('email', 'date_of_birth', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(MyUser, UserAdmin)
+admin.site.unregister(Group)
--- myuser/models.py
+++ myuser/models.py
@@ -1,47 +1,72 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager , AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
 
 class MyUserManager(BaseUserManager):
-		def create_user(self,email,date_of_birth,password=None):
-				if not email:
-						raise ValueError('Email must be set')
-				user=self.model(email=email,date_of_birth=date_of_birth)
-				user.set_password(password)
-				user.save(using=self._db)
-				return user
-		def create_superuser(self,email,date_of_birth,password):
-				user=self.create_user(email,date_of_birth=date_of_birth,password=password)
-				user.is_admin=True
-				user.save(using=self._db)
-				return user
+    def create_user(self, email, date_of_birth, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
 
 class MyUser(AbstractBaseUser):
-		email=models.EmailField(unique=True)
-		date_of_birth=models.DateField()
-		is_active=models.BooleanField(default=True)
-		is_admin=models.BooleanField(default=False)
-		
-		objects=MyUserManager
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
-		USERNAME_FIELD="email"
-		
-		REQUIRED_FIELDS=['date_of_birth']
+    objects = MyUserManager()
 
-		def get_short_name(self):
-				return self.email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
 
-		def get_full_name(self):
-				return self.email
-		
-		def  has_perms(self,perm,ob=None):
-				return True
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
 
-		def has_module_perms(self,app_label):
-				return True
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
 
-		@property
-		def is_staff(self):
-				self.is_admin
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
 
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
 
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
 
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
