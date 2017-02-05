3ab1cbc1c75b2e394b852c965a73b74a84875839
AlbertStepanyan <1felisidad1@gmail.com>
2017-02-05 22:36:50 +0400

    Add captcha field to field list

--- myuser/admin.py
+++ myuser/admin.py
@@ -45,7 +45,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'date_of_birth', 'password1', 'password2')}
+            'fields': ('email', 'date_of_birth', 'password1', 'password2','captcha')}
         ),
     )
     search_fields = ('email',)
